1cf84ce8d9eed11ba9db196ca74d8aeb1693d502
muhammad sohail nawaz <swesohailjanjua786@gmail.com>
2025-01-09 17:56:27 -0800

    set router and darkmode

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,58 @@
+import React,{useState} from 'react'
 import './App.css'
+import About from './components/About'
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm'
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 function App() {
- 
+const[mode, setMode]=useState('light')
+const[btnText, setBtnText]=useState('Enable Dark Mode')
+
+
+const toggleMode=()=>{
+  if(mode==='light'){
+    setMode('dark')
+    document.body.style.backgroundColor='#23272f'
+    setBtnText('Enable Light Mode')
+//     setInterval(()=>{
+// document.title="Textustils is amazing web app"
+//     },2000)
+//     setInterval(() => {
+//       document.title="Install textUtils now"
+//     }, 1500);
+  }
+  else{
+    setMode('light')
+    document.body.style.backgroundColor='white'
+    setBtnText('Enable Dark Mode')
+  }
+}
 
   return (
     <>
 {/* with props */}
- <Navbar title="TextUtils" aboutText='About textutls'/>
  {/* if i set default props */}
  {/* <Navbar /> */}
-
 {/* set about by default */}
  {/* <Navbar title="TextUtils"/> */}
- <div className='container my-3'>
- <TextForm  heading="Enter the text to analyze"/>
- </div>
 
+<Router>
+ <Navbar title="TextUtils" aboutText='About textutls' mode={mode} toggleMode={toggleMode} btnText={btnText}/>
+ <div className='container my-3'>
+ <Routes>
+
+ <Route path="/about" element={<About />} />
+ <Route path="/" element={<TextForm heading="Enter the text to analyze" mode={mode} />} />
+
+        </Routes>
+ </div>
+ </Router>
     </>
   )
 }
--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -18,6 +18,11 @@ const handleRemoveExtraSpaces = () => {
     setText(newText);
 };
 
+const deleteText=()=>{
+  setText("");
+  confirm("Are you sure you want to delete these text")
+}
+
 const handleOnchange=(event)=>{
     // console.log("value changed")
     setText(event.target.value)
@@ -26,18 +31,29 @@ const handleOnchange=(event)=>{
 
 
 
-    const[text,setText]=useState("Enter text here");
+    const[text,setText]=useState("");
   return (
-<div>
+    <>
+<div className='container'style={{color:props.mode==='dark'?'white':'black'}}>
 <h1>{props.heading}</h1>
 <div className="mb-3">
-  <textarea className="form-control" value={text} onChange={handleOnchange} id="myBox" rows="8"></textarea>
+  <textarea className="form-control" value={text} onChange={handleOnchange} id="myBox" rows="8"style={{backgroundColor:props.mode
+    ==='dark' ? '#23272f':'white', color:props.mode==='dark'?'white':'black'
+  }}></textarea>
 </div>
 <button className='btn btn-primary' onClick={handConvertUp} >Convert To Upercaase</button>
 <button className='btn btn-primary ms-3' onClick={handConvertLow}>Convert To Lowercase</button>
 <button className='btn btn-primary ms-3' onClick={handleRemoveExtraSpaces}>Remove extra space</button>
-
+<button className='btn btn-primary ms-3' onClick={deleteText}>Delete all text</button>
 </div>
+<div className="container my-5"style={{color:props.mode==='dark'?'white':'black'}}>
+  <h1>Your text Summary here:--</h1>
+  <p>{text.split(" ").length} word and {text.length} character</p>
+  <p>{0.008 * text.split(" ").length} Minutes read...</p>
+  <h1>Preview</h1>
+  <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
+</div>
+</>
   )
 }
 TextForm.prototypes={
